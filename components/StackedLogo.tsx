fa315d63c7572f855cebf5b2f9f4029005071da7
Tom McGurl <tom.mcgurl@gmail.com>
2025-05-24 00:09:45 -0400

    Logo animated

--- components/StackedLogo.tsx
+++ components/StackedLogo.tsx
@@ -1,4 +1,4 @@
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 export default function StackedLogo({
@@ -58,131 +58,114 @@ export default function StackedLogo({
 
   return (
     <motion.div
-      initial={{ opacity: 1 }}
-      animate={{ opacity: 1 }}
       onHoverStart={() => setIsHovered(true)}
       onHoverEnd={() => setIsHovered(false)}
-      whileHover={{ scale: 1.05 }}
-      transition={{ scale: { duration: 0.3 } }}
-      className="cursor-pointer"
       style={{
         position: "relative",
         width: 500,
         height: 240,
         margin: "0 auto",
+        cursor: "pointer",
       }}
     >
-      <AnimatePresence mode="wait">
-        {!isHovered ? (
-          // Stacked logo (shown when not hovered)
-          <motion.div
-            key="stacked"
-            initial={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "240px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {/* T */}
+        <motion.div
+          animate={{
+            x: isHovered ? "-83%" : 0,
+          }}
+          transition={{
+            duration: 0.5,
+            ease: "easeOut",
+          }}
+          style={{
+            position: "absolute",
+            width: 240,
+            height: 240,
+            left: "50%",
+            top: 0,
+            marginLeft: -120,
+            zIndex: 2,
+          }}
+        >
+          <img
+            src="/T.svg"
+            alt="T"
             style={{
-              position: "relative",
-              width: "240px",
-              height: "240px",
-              margin: "0 auto",
-            }}
-          >
-            <img
-              src="/T.svg"
-              alt="T"
-              style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                width: "100%",
-                height: "100%",
-              }}
-            />
-            <img
-              src="/M.svg"
-              alt="M"
-              style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                width: "100%",
-                height: "100%",
-              }}
-            />
-            <img
-              src="/O.svg"
-              alt="O"
-              style={{
-                position: "absolute",
-                left: "50%",
-                bottom: "5%",
-                width: "40%",
-                height: "40%",
-                transform: "translateX(-50%)",
-              }}
-            />
-          </motion.div>
-        ) : (
-          // Separated T-O-M letters (shown when hovered)
-          <motion.div
-            key="separated"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
-            style={{
-              position: "relative",
               width: "100%",
               height: "100%",
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
             }}
-          >
-            <motion.div
-              initial={{ x: 50, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 0.5, ease: "easeOut" }}
-              whileHover={{ scale: 1.1 }}
-              style={{ width: "30%", height: "100%" }}
-            >
-              <img
-                src="/T.svg"
-                alt="T"
-                style={{ width: "100%", height: "100%" }}
-              />
-            </motion.div>
+          />
+        </motion.div>
 
-            <motion.div
-              initial={{ y: 20, scale: 0.8, opacity: 0 }}
-              animate={{ y: 0, scale: 1, opacity: 1 }}
-              transition={{ duration: 0.5, ease: "easeOut", delay: 0.1 }}
-              whileHover={{ scale: 1.1 }}
-              style={{ width: "30%", height: "100%" }}
-            >
-              <img
-                src="/O.svg"
-                alt="O"
-                style={{ width: "100%", height: "100%" }}
-              />
-            </motion.div>
+        {/* O */}
+        <motion.div
+          animate={{
+            y: isHovered ? 30 : 55,
+            scale: isHovered ? 0.6 : 0.4,
+          }}
+          transition={{
+            duration: 0.5,
+            ease: "easeOut",
+          }}
+          style={{
+            position: "absolute",
+            width: 240,
+            height: 240,
+            left: "50%",
+            top: 0,
+            marginLeft: -120,
+            zIndex: 3,
+          }}
+        >
+          <img
+            src="/O.svg"
+            alt="O"
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        </motion.div>
 
-            <motion.div
-              initial={{ x: -50, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 0.5, ease: "easeOut", delay: 0.2 }}
-              whileHover={{ scale: 1.1 }}
-              style={{ width: "30%", height: "100%" }}
-            >
-              <img
-                src="/M.svg"
-                alt="M"
-                style={{ width: "100%", height: "100%" }}
-              />
-            </motion.div>
-          </motion.div>
-        )}
-      </AnimatePresence>
+        {/* M */}
+        <motion.div
+          animate={{
+            x: isHovered ? "83%" : 0,
+          }}
+          transition={{
+            duration: 0.5,
+            ease: "easeOut",
+          }}
+          style={{
+            position: "absolute",
+            width: 240,
+            height: 240,
+            left: "50%",
+            top: 0,
+            marginLeft: -120,
+            zIndex: 1,
+          }}
+        >
+          <img
+            src="/M.svg"
+            alt="M"
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        </motion.div>
+      </div>
     </motion.div>
   );
 }
